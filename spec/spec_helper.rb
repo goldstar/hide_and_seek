77d310fbad7f634d182a6057fa83bd400c5700b4
mnicoq <nicolas.quintana@goldstar.com>
2022-04-11 11:52:54 -0300

    Fix CI
    Fix conflict rails/rails#37048. Ref
    https://github.com/goldstar/hide_and_seek/pull/24

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,13 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path("../internal/config/environment.rb",  __FILE__)
-
-require 'rubygems'
 require 'bundler/setup'
 Bundler.setup
 
-require 'redis'
-
-require 'rspec/rails'
-
-Rails.backtrace_cleaner.remove_silencers!
-
 require 'combustion'
 Combustion.initialize! :all
+
+require 'rspec/rails'
+require 'redis'
+
+Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
@@ -23,7 +18,4 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
   config.disable_monkey_patching!
-end
-
-RSpec.configure do |config|
 end
